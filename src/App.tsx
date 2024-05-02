fc9a39b3d5d359cd64d432da250da38ba354acc0
gustavoUfrn <mxgugaa@gmail.com>
2024-05-02 15:02:25 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from './components/NavBar'
+import Header from './components/Navbar'
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './app.css'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
